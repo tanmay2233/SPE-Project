cc2f8814611255d22af775731d6d38e4178c8dae
tanmay2233 <tanmaykamleshjain@gmail.com>
2024-09-26 15:50:19 +0530

    final commit

--- src/main/java/spe/Main.java
+++ src/main/java/spe/Main.java
@@ -1,14 +1,9 @@
 package org.example;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.util.Scanner;
 import java.math.BigDecimal;
 
 public class Main {
-
-    private static final Logger logger = LogManager.getLogger(Main.class); 
 
     public static String squareRoot(double x) {
         if (x < 0) {
